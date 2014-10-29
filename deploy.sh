245196279f2b7eaa9fea563ee33025acbcf38e0a
Frank LSF <franklsf95@gmail.com>
2014-10-28 20:05:52 -0500

    Added responsive support for sketch page

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 echo "$(tput setaf 2)Building Jekyll... $(tput sgr0)"
 jekyll build
 echo "$(tput setaf 2)Syncing with AWS S3... $(tput sgr0)"
-aws s3 sync _site s3://sketch-off.com/
+aws s3 sync _site s3://sketch-off.com/ --region us-west-2
